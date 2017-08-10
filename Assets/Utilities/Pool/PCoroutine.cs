9fe488796e51929221d1cbce6bd66254cd17155f
Patrick <patrick.heney@gmail.com>
2017-08-10 09:49:06 -0500

    Alternate PCoroutine runner
    Try CreateCoroutineRunner(IEnumerator) and see how that works. Does it run the corouting parameter and then self-destruct upon completetion?

--- Assets/Utilities/Pool/PCoroutine.cs
+++ Assets/Utilities/Pool/PCoroutine.cs
@@ -7,6 +7,8 @@ namespace PLib.Pooling
     {
         private Coroutine c;
 
+        #region Original
+
         /// <summary>
         /// 2017-8-8
         /// Creates a temporary GameObject and attaches a PCoroutine 
@@ -31,28 +33,10 @@ namespace PLib.Pooling
         /// </summary>
         public void StartCoroutineDelegate(IEnumerator ienumerator)
         {
-            //c = StartCoroutine(ienumerator);
-            //StartCoroutine(DestroyWhenFinished());
-            StartCoroutine(DestroyWhenFinished(ienumerator));
+            c = StartCoroutine(ienumerator);
+            StartCoroutine(DestroyWhenFinished());
         }
 
-        /// <summary>
-        /// 2017-8-8
-        /// Monitors the status of the running coroutine by checking 
-        /// every few seconds (1~3). Once the coroutine has terminated,
-        /// this destroys this GameObject.
-        /// </summary>
-        private IEnumerator DestroyWhenFinished(IEnumerator ienumerator)
-        {
-            IEnumerator routine = ienumerator;
-            while (routine != null)
-            {
-                yield return ienumerator;
-            }
-
-            if (Application.isEditor) DestroyImmediate(gameObject);
-            else Destroy(gameObject, Random.value + 0.1f);
-        }
 
         /// <summary>
         /// 2017-8-8
@@ -70,5 +54,45 @@ namespace PLib.Pooling
             if (Application.isEditor) DestroyImmediate(gameObject);
             else Destroy(gameObject, Random.value + 0.1f);
         }
+
+        #endregion
+        #region Refactored
+
+        /// <summary>
+        /// 2017-8-10
+        /// Creates a temporary GameObject and attaches a PCoroutine 
+        /// MonoBehaviour. This is used to run coroutines for delayed
+        /// actions. The GameObject deletes itself once complete.
+        /// </summary>
+        public static void CreateCoroutineRunner(IEnumerator ienumerator, string name = "")
+        {
+            //  Create a game object with no renderer or geometry
+            GameObject g = new GameObject("_CoroutineRunner_" + name + "_" + Time.time);
+
+            //  Attach a PCoroutine MonoBehavior to the GameObject
+            PCoroutine pcoroutine = g.AddComponent<PCoroutine>();
+
+            //  Start the coroutine
+            pcoroutine.StartCoroutine(pcoroutine.DestroyWhenFinished(ienumerator));
+        }
+        
+        /// <summary>
+        /// 2017-8-10
+        /// Monitors the status of the running coroutine by checking 
+        /// every few seconds (1~3). Once the coroutine has terminated,
+        /// this destroys this GameObject.
+        /// </summary>
+        private IEnumerator DestroyWhenFinished(IEnumerator ienumerator)
+        {
+            IEnumerator routine = ienumerator;
+            while (routine != null)
+            {
+                yield return ienumerator;
+            }
+
+            if (Application.isEditor) DestroyImmediate(gameObject);
+            else Destroy(gameObject, Random.value + 0.1f);
+        }
+        #endregion
     }
 }
